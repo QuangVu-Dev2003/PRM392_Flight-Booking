02a56c405d387b5013b021b17f28ecce0638385a
PhucPM <minhphuc2308031@gmail.com>
2025-07-06 21:57:23 +0700

    source base UI

---
+++ UI/FlightBooking/app/src/main/java/com/prm/flightbooking/ChooseSeatsActivity.java
@@ -0,0 +1,14 @@
+package com.prm.flightbooking;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChooseSeatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_seats);
+    }
+}
