e8404d0528c8a712fec7d7f55c9a36af449ce1ba
PhucPM <minhphuc2308031@gmail.com>
2025-07-07 21:34:03 +0700

    Auth API and EmailService

---
+++ API/FlightBooking/Service/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿namespace FlightBooking.Services
+{
+    public interface IEmailService
+    {
+        Task SendBookingConfirmationEmailAsync(Models.Booking booking);
+        Task SendFlightUpdateEmailAsync(string email, string subject, string message);
+        Task SendCancellationEmailAsync(Models.Booking booking);
+        Task SendPaymentConfirmationEmailAsync(Models.Payment payment);
+    }
+}
